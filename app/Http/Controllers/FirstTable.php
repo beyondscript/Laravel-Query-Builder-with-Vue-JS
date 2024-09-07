28a10def45e278a2e94bf9da569ae6938bb9c53b
Beyondscript <nafiul1351@gmail.com>
2024-09-07 12:36:04 +0600

    initial commit

---
+++ app/Http/Controllers/FirstTable.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use DB;
+use Image;
+
+class FirstTable extends Controller
+{
+    public function store(Request $req){
+        $validated = $req->validate([
+            'image' => ['image']
+        ],
+        [
+            'image.image' => 'Image must be an image'
+        ]);
+
+        $data = array();
+        $image = request()->file('image');
+        if($image){
+            $name = hexdec(uniqid());
+            $fullname = $name.'.webp';
+            $path = 'images/firsttable/';
+            $url = $path.$fullname;
+            $resize_image=Image::make($image->getRealPath());
+            $resize_image->resize(300,300);
+            $resize_image->save('images/firsttable/'.$fullname);
+            $data['text'] = $req -> text;
+            $data['number'] = $req -> number;
+            $data['image'] = $url;
+            DB::table('firsttable') -> insert($data);
+            return response()->json([
+                'message' => 'Data successfully added'
+            ],200);
+        }
+        else{
+            $data['text'] = $req -> text;
+            $data['number'] = $req -> number;
+            DB::table('firsttable') -> insert($data);
+            return response()->json([
+                'message' => 'Data successfully added'
+            ],200);
+        }
+    }
+    public function show(){
+        $firsttables = DB::table('firsttable') -> orderBy('text', 'ASC') -> get();
+        return $firsttables;
+    }
+    public function edit($id){
+        $firsttable = DB::table('firsttable') -> where('id', $id)->first();
+        return $firsttable;
+    }
+    public function update(Request $req){
+        $validatedData = $req->validate([
+            'image' => ['image']
+        ],
+        [
+            'image.image' => 'Image must be an image'
+        ]);
+
+        $data = array();
+        $image = request()->file('image');
+        if($image){
+            $firsttable = DB::table('firsttable') -> where('id', $req->id)->first();
+            $old_image=$firsttable->image;
+            if(file_exists($old_image)){
+                unlink($old_image);
+            }
+            $name = hexdec(uniqid());
+            $fullname = $name.'.webp';
+            $path = 'images/firsttable/';
+            $url = $path.$fullname;
+            $resize_image=Image::make($image->getRealPath());
+            $resize_image->resize(300,300);
+            $resize_image->save('images/firsttable/'.$fullname);
+            $data['text'] = $req -> text;
+            $data['number'] = $req -> number;
+            $data['image'] = $url;
+            DB::table('firsttable') -> where('id', $req->id)->update($data);
+            return response()->json([
+                'message' => 'Data successfully updated'
+            ],200);
+        }
+        else{
+            $data['text'] = $req -> text;
+            $data['number'] = $req -> number;
+            DB::table('firsttable') -> where('id', $req->id)->update($data);
+            return response()->json([
+                'message' => 'Data successfully updated'
+            ],200);
+        }
+    }
+    public function destroy(Request $req){
+        $firsttable = DB::table('firsttable') -> where('id', $req->id)->first();
+        $image  = $firsttable->image;
+        if(file_exists($image)){
+            unlink($image);
+        }
+        DB::table('firsttable') -> where('id', $req->id)->delete();
+        return response()->json([
+            'message' => 'Data successfully deleted'
+        ],200);
+    }
+}
